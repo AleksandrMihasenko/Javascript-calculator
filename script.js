8858ad383ca387f6072a1205a706c31b1bcf8089
AleksandrMihasenko <aleksandr.mihasenko@gmail.com>
2020-05-31 17:06:27 +0300

    add the main functionality

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const answer = document.querySelector('.answer');
 const history = document.querySelector('.history');
 
 let displayHistory = '0';
-let displayNum = '0';
+let displayAnswer = '0';
+
 let firstNum = '';
 let secondNum = '';
 let showHistory = '';
@@ -12,29 +13,78 @@ let operator = '';
 let regExp = /\d/;
 
 
-answer.innerHTML = displayNum;
+answer.innerHTML = displayAnswer;
 history.innerHTML = displayHistory;
 
 buttons.addEventListener('click', event => {
     
-    if (regExp.test(event.target.getAttribute('data-value')) && firstNum.length < 6) {
-        setNum();     
-        
+    if (regExp.test(event.target.getAttribute('data-value')) && showHistory.length < 6 && firstNum.length < 6 && !operator) {
+
+        setFirstNum();    
+
+    } else if (regExp.test(event.target.getAttribute('data-value')) && secondNum.length < 6 && operator) {
+
+        setSecondNum();
+
     } else if (event.target.getAttribute('data-value') === 'c') {
         clearAll();
     } else if (event.target.getAttribute('data-value') === '.') {
         console.log('dot');
-    } 
+    } else if (event.target.getAttribute('data-value') === '=') {
+        
+        calculateResult();
+
+    } else if (firstNum && !regExp.test(event.target.getAttribute('data-value')) && !operator) {
+
+        setSign();
+
+    }
 });
 
-function setNum() {
+function setFirstNum() {
     firstNum += event.target.getAttribute('data-value');    
-    answer.innerHTML = firstNum;
-    history.innerHTML = firstNum;
+    answer.innerHTML = firstNum;      
+    history.innerHTML = firstNum;    
+}
+
+function setSecondNum() {
+    secondNum += event.target.getAttribute('data-value');    
+    answer.innerHTML = secondNum;
+    showHistory = firstNum + ' ' + operator + ' ' + secondNum;
+    history.innerHTML = showHistory;
+}
+
+function setSign() {
+    operator = event.target.getAttribute('data-value');
+    showHistory = firstNum + ' ' + operator;
+    history.innerHTML = showHistory;    
+}
+
+function calculateResult() {
+    switch (operator) {
+        case '+': 
+            result = parseInt(firstNum) + parseInt(secondNum);
+            answer.innerHTML = result;            
+            break;
+        case '-': 
+            result = parseInt(firstNum) - parseInt(secondNum);
+            answer.innerHTML = result;
+            break;
+        case 'x': 
+            result = parseInt(firstNum) * parseInt(secondNum);
+            answer.innerHTML = result;
+            break;
+        case '/': 
+            result = parseInt(firstNum) / parseInt(secondNum);
+            answer.innerHTML = result;
+            break;
+    }
 }
 
 function clearAll() {
     firstNum = '';
-    answer.innerHTML = displayNum;
+    secondNum = '';
+    operator = '';
+    answer.innerHTML = displayAnswer;
     history.innerHTML = displayHistory;
 }
